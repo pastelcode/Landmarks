c5e95412304f27e30462ec3fbeca91afd50bb437
Pastel <pastel.dev.gt@gmail.com>
2023-12-09 13:51:02 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Samuel Marroquín on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
